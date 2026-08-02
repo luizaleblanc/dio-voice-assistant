1915020e13ff73be26e24517258fb5420f8d7652
Luiza Ferraz <luizaa.fq@gmail.com>
2026-08-02 12:04:55 -0300

    Remove arquivo UserRepository antigo que ficou esquecido no commit anterior

--- src/main/java/dio/budgeting/infrastructure/persistence/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dio.budgeting.infrastructure.persistence;
-
-import dio.budgeting.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
